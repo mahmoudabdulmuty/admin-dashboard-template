60b715ff03bc4d49cb07dce886b304f68c6c0df0
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-03-31 16:42:48 +0200

    update

--- js/darkMode.js
+++ js/darkMode.js
@@ -5,7 +5,7 @@ moonIcon.addEventListener('click', () => {
   if (sunIcon.style.display === 'none') {
     moonIcon.style.display = 'none';
     sunIcon.style.display = '';
-    document.body.classList.add('dark-mode');
+    document.body.setAttribute('id', 'dark-mode');
   }
 });
 
@@ -13,6 +13,6 @@ sunIcon.addEventListener('click', () => {
   if (moonIcon.style.display === 'none') {
     moonIcon.style.display = '';
     sunIcon.style.display = 'none';
-    document.body.classList.remove('dark-mode');
+    document.body.removeAttribute('id', 'dark-mode');
   }
 });
